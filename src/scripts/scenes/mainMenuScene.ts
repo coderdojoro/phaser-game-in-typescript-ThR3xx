3f3e60e558788d23c0c7f6bce10e58e829557599
Luca Dumitru <thr3x.official@gmail.com>
2021-12-18 11:01:34 +0200

    finish lesson 18 dec

--- src/scripts/scenes/mainMenuScene.ts
+++ src/scripts/scenes/mainMenuScene.ts
@@ -1,13 +1,15 @@
 import 'phaser';
 
 export default class MainMenuScene extends Phaser.Scene {
-
+    rightKey:Phaser.Input.Keyboard.Key;
+    hero:Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     constructor() {
         super({ key: 'MainMenuScene' });
     }
 
     preload() {
-        this.load.spritesheet('idle-e-spirtesheet', 'assets/hero/idle_aggro_E.png', {frameWidth:128, frameHeight:128});
+        this.load.spritesheet('idle-e-spritesheet', 'assets/hero/idle_aggro_E.png', {frameWidth:128, frameHeight:128});
+        this.load.spritesheet('walk-e-spritesheet', 'assets/hero/walk_aggro_E.png', {frameWidth:128, frameHeight:128});
     }
 
     create() {
@@ -26,19 +28,32 @@ export default class MainMenuScene extends Phaser.Scene {
 
         this.cameras.main.fadeIn(2000);
         this.cameras.main.setBackgroundColor('#008080');
-    
-        let hero=this.physics.add.sprite(200,200,'idle-e-spirtesheet',0);
+        this.rightKey=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.hero=this.physics.add.sprite(200,200,'idle-e-spirtesheet',0);
         this.anims.create({
             key:'idle-e-anim',
-            frames:this.anims.generateFrameNumbers('idle-e-spirtesheet', {}),
+            frames:this.anims.generateFrameNumbers('idle-e-spritesheet', {}),
             frameRate:10,
             repeat:-1
         });
-        hero.anims.play('idle-e-anim');
+        this.anims.create({
+            key:'walk-e-anim',
+            frames:this.anims.generateFrameNumbers('walk-e-spritesheet', {}),
+            frameRate:10,
+            repeat:-1
+        });
+        this.hero.anims.play('idle-e-anim');
 
     }
 
     update(time, delta) {
-        
+        this.hero.body.setVelocity(0);
+        if(this.rightKey.isDown){
+            this.hero.body.setVelocityX(175);
+            this.hero.anims.play('walk-e-anim',true)
+        }
+        else{
+            this.hero.anims.play('idle-e-anim',true);
+        }
     }
 }
