6b17d9b2a7232930d5f2bfd484f7745005801d30
Mihai Vasilache <mihai.vasilache@gmail.com>
2022-04-09 09:22:53 +0300

    update

--- src/scripts/entities/grizzly.ts
+++ src/scripts/entities/grizzly.ts
@@ -1,11 +1,11 @@
 import 'phaser';
 import * as EasyStar from 'easystarjs';
 import MainMenuScene from '../scenes/mainMenuScene';
-export default class Grizzly extends Phaser.GameObjects.Sprite{
+export default class Grizzly extends Phaser.GameObjects.Sprite {
     easystar: EasyStar.js;
-    scene: MainMenuScene;
+    scene:MainMenuScene;
     constructor(scene, x, y) {
-        super(scene, x, y, 'grizzly-idle-spritesheet', 0); 
+        super(scene, x, y, 'idle-e-spritesheet', 0);
         this.scene = scene;
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
@@ -16,14 +16,24 @@ export default class Grizzly extends Phaser.GameObjects.Sprite{
             key: 'grizzly-idle-anim',
             frames: this.anims.generateFrameNumbers('grizzly-idle-spritesheet', {}),
             frameRate: 5,
-            repeat: -1
-        });
+            repeat: -1,
+        })
         this.anims.play('grizzly-idle-anim', true);
 
         this.easystar = new EasyStar.js();
-        this.easystar.setGrid(this.scene.worldlayer.layer.data.map((arr)=>arr.map((tile)=>tile.index)));
+        this.easystar.setGrid(this.scene.worldLayer.layer.data.map((arr)=>arr.map((tile)=> tile.index)));
         this.easystar.setAcceptableTiles(-1);
         this.easystar.enableDiagonals();
         this.easystar.enableCornerCutting();
-    }    
+
+        
+    }
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+        let distance = Phaser.Math.Distance.Between(this.x, this.y, this.scene.hero.x, this.scene.hero.y);
+        if(distance<300){
+            console.log("moving");
+        }
+    }
 }
+
--- src/scripts/scenes/mainMenuScene.ts
+++ src/scripts/scenes/mainMenuScene.ts
@@ -4,7 +4,7 @@ import Hero from '../entities/hero';
 
 export default class MainMenuScene extends Phaser.Scene {
     hero: Hero;
-    worldlayer: Phaser.Tilemaps.TilemapLayer;
+    worldLayer: Phaser.Tilemaps.TilemapLayer;
     constructor() {
         super({ key: 'MainMenuScene' });
     }
@@ -20,11 +20,11 @@ export default class MainMenuScene extends Phaser.Scene {
         this.load.spritesheet('atk-e-spritesheet', 'assets/hero/atk_heavy_E.png', { frameWidth: 128, frameHeight: 128 });
         this.load.spritesheet('atk-s-spritesheet', 'assets/hero/atk_heavy_S.png', { frameWidth: 128, frameHeight: 128 });
 
-        this.load.spritesheet('grizzly-idle-spritesheet', 'assets/enemies/grizzly-idle.png', { frameWidth: 32, frameHeight: 32 });
-        this.load.spritesheet('grizzly-walk-n-spritesheet', 'assets/enemies/grizzly-north.png', { frameWidth: 32, frameHeight: 32 });
-        this.load.spritesheet('grizzly-walk-s-spritesheet', 'assets/enemies/grizzly-south.png', { frameWidth: 32, frameHeight: 32 });
-        this.load.spritesheet('grizzly-walk-e-spritesheet', 'assets/enemies/grizzly-east.png', { frameWidth: 32, frameHeight: 32 });
-        this.load.spritesheet('grizzly-die-spritesheet', 'assets/enemies/grizzly-die.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('grizzly-idle-spritesheet', 'assets/entities/grizzly-idle.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('grizzly-walk-n-spritesheet', 'assets/entities/grizzly-north.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('grizzly-walk-s-spritesheet', 'assets/entities/grizzly-south.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('grizzly-walk-e-spritesheet', 'assets/entities/grizzly-east.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('grizzly-die-spritesheet', 'assets/entities/grizzly-die.png', { frameWidth: 32, frameHeight: 32 });
         this.load.image('tiles', 'assets/tilesets/ground-tileset.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/town.json');
     }
@@ -50,9 +50,9 @@ export default class MainMenuScene extends Phaser.Scene {
 
         let belowLayer = map.createLayer('Below hero', tileset, 0, 0);
         let objBelowLayer = map.createLayer('Objects below hero', tileset, 0, 0);
-        this.worldlayer = map.createLayer('World', tileset, 0, 0);
+        this.worldLayer = map.createLayer('World', tileset, 0, 0);
         let abovelayer = map.createLayer('Above hero', tileset, 0, 0);
-        this.worldlayer.setCollisionBetween(tileset.firstgid, tileset.firstgid + tileset.total, true);
+        this.worldLayer.setCollisionBetween(tileset.firstgid, tileset.firstgid + tileset.total, true);
 
         let spawnPoint = map.findObject('Objects', (obj) => obj.name == 'Spawn Point');
 
@@ -60,13 +60,13 @@ export default class MainMenuScene extends Phaser.Scene {
 
         let grizzlyObjects = map.getObjectLayer('Objects').objects.filter((elem) => elem.name == 'grizzly');
         for (let grizzly of grizzlyObjects) {
-            new Grizzly(this, grizzly.x, grizzly.y);
-            
+        new Grizzly(this,grizzly.x, grizzly.y);            
         }
+
         abovelayer.setDepth(100);
         this.hero.setDepth(50);
 
-        this.physics.add.collider(this.hero, this.worldlayer);
+        this.physics.add.collider(this.hero, this.worldLayer);
         this.cameras.main.startFollow(this.hero);
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
